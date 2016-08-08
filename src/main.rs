0627521ab19f069bf25c54d0a5ec85fa3d2e3b95
Rob Bolton <rab26@aber.ac.uk>
2016-08-08 20:11:41 +0100

    Got it working, uses backtracking.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::char::from_digit;
+use std::collections::HashSet;
 use std::io::{Read, BufReader, BufRead};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -50,11 +51,45 @@ impl Board {
     fn patch(&self, x: usize, y: usize, cell: Cell) -> Self {
         let mut grid = self.grid.clone();
 
-        grid[x][y] = cell;
+        grid[y][x] = cell;
         
         Board {
             grid: grid,
         }
+    }
+
+    fn get_possible(&self, x: usize, y: usize) -> HashSet<u8> {
+        let mut nums: HashSet<u8> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9].into_iter().collect();
+        for x in 0..9 {
+            match self.grid[y][x] {
+                Cell::Fixed(num) | Cell::Maybe(num) => {
+                    nums.remove(&num);
+                },
+                Cell::Open => continue,
+            }
+        }
+        for y in 0..9 {
+            match self.grid[y][x] {
+                Cell::Fixed(num) | Cell::Maybe(num) => {
+                    nums.remove(&num);
+                },
+                Cell::Open => continue,
+            }
+        }
+        let grid_x = x / 3;
+        let grid_y = y / 3;
+        for y in 0 .. 3 {
+            for x in 0 .. 3 {
+                match self.grid[y + (grid_y * 3)][x + (grid_x * 3)] {
+                    Cell::Fixed(num) | Cell::Maybe(num) => {
+                        nums.remove(&num);
+                    },
+                    Cell::Open => continue,
+                }      
+            }
+        }
+
+        nums
     }
 
     fn parse_puzzle<T: Read>(input: T) -> Self {
@@ -85,6 +120,7 @@ impl Board {
     }
 }
 
+
 impl ToString for Board {
     fn to_string(&self) -> String {
         let mut board_str = String::new();
@@ -110,9 +146,49 @@ impl ToString for Board {
     }
 }
 
-
 fn main() {
+    let board = Board::parse_puzzle(std::io::stdin());
+    if let Some(solved) = backtrack(board, 0, 0) {
+        println!("{}", solved.to_string());
+    } else {
+        println!("Could not solve.");
+    }
+}
+
+fn backtrack(board: Board, x: usize, y: usize) -> Option<Board> {
+    let (next_x, next_y) = next_coords(x, y);
+    if y == 9 {
+        return Some(board);
+    }
+    match board.grid[y][x] {
+        Cell::Fixed(_) | Cell::Maybe(_) => {
+            backtrack(board, next_x, next_y)
+        },
+        Cell::Open => {
+            let nums = board.get_possible(x, y);
+            
+            for num in nums.iter() {
+                if let Some(solved) = backtrack(board.patch(x, y, Cell::Maybe(*num)), next_x, next_y) {
+                    return Some(solved);
+                }
+            }
+
+            None
+        },
+    }
+}
+
+fn next_coords(x: usize, y: usize) -> (usize, usize) {
+    let mut y = y;
+    let x = match x {
+        8 => {
+            y = y + 1;
+            0
+        },
+        _ => x + 1
+    };
     
+    (x, y)
 }
 
 
@@ -121,7 +197,7 @@ fn main() {
 fn test_patch() {
     let board = Board::new(
         vec!((0, 0, 1))
-    ).patch(0, 1, Cell::Fixed(2));
+    ).patch(1, 0, Cell::Fixed(2));
 
     assert_eq!(Cell::Fixed(1), board.grid[0][0]);
     assert_eq!(Cell::Fixed(2), board.grid[0][1]);
@@ -142,8 +218,28 @@ fn test_parse_puzzle() {
 ";
     
     let puzzle = Board::parse_puzzle(puzzle_str.as_bytes());
-    println!(":\n{}\n", puzzle_str);
-    println!(":\n{}\n", puzzle.to_string());
     assert_eq!(puzzle_str, puzzle.to_string());
 }
-        
+
+#[test]
+fn test_coords() {
+    assert_eq!((1, 0), next_coords(0, 0));
+    assert_eq!((0, 1), next_coords(8, 0));
+}
+
+#[test]
+fn test_get_possible() {
+    let board = Board::parse_puzzle(
+" 23456789
+2        
+3        
+4        
+5        
+6        
+7        
+8        
+9        ".as_bytes());
+
+    let expected: HashSet<u8> = vec![1].into_iter().collect();
+    assert_eq!(expected, board.get_possible(0, 0));
+}
